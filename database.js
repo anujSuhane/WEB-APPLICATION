66898c12691f9df04b0a55cc76ce2c809a3f47bf
Anuj <omsuhane6262@gmail.com>
2023-12-16 14:48:05 +0530

    ORDER HEADER

--- database.js
+++ database.js
@@ -3,6 +3,10 @@
  const pool = mysql.createPool({
     host : '127.0.0.1',
     user: 'root',
-    password: '',
+    password: 'sql1234',
     database: 'orders'
  }).promise()
+ 
+const create_order_header = 'CREATE TABLE order_header(ORDER_ID VARCHAR(40) NOT NULL PRIMARY KEY, ORDER_NAME VARCHAR(255) DEFAULT NULL, PLACED_DATE datetime DEFAULT NULL, STATUS_ID varchar(40) DEFAULT NULL, CURRENCY_UOM_ID varchar(40) DEFAULT NULL, PRODUCT_STORE_ID varchar(40) DEFAULT NULL, SALES_CHANNEL_ENUM_ID varchar(40) DEFAULT NULL, GRAND_TOTAL decimal(24,4) DEFAULT NULL, COMPLETED_DATE datetime NULL) ';
+
+pool.query(create_order_header);
